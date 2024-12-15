c58e7d19b590eb8545d04b3bfd4db22697b5c138
kunzl <kunzl@Lukas-Nitro-5>
2024-12-15 23:40:04 +0100

    delete

--- initInputs.m
+++
@@ -1,6 +0,0 @@
-function [TotalThrust, uMoment1, uMoment2, uMoment3] = initInputs(inputArg1,inputArg2)
-%INITINPUT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
